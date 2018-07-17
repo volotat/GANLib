4c30d088003219506f1dbea7cd778df77a3e623a
Alex K <BangbangfmGames@gmail.com>
2018-07-17 19:55:44 +0300

    ProgGAN update

--- tests/ProgGAN_test.py
+++ tests/ProgGAN_test.py
@@ -2,7 +2,7 @@ from GANLib import ProgGAN
 from GANLib import plotter
 
 from keras.datasets import mnist, fashion_mnist, cifar10
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, concatenate
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, concatenate, RepeatVector, UpSampling2D
 from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D, AveragePooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
@@ -11,6 +11,46 @@ from keras.optimizers import Adam, RMSprop, Nadam
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def build_generator(self):
+    input_layer = Input(shape=(self.latent_dim,))
+    layer = RepeatVector(16)(input_layer)
+    layer = Reshape((4, 4, self.latent_dim))(layer)
+    
+    layer = Conv2D(self.latent_dim, (4,4), padding='same', weights = self.genr_head_weights, name = 'genr_head')(layer)
+    layer = LeakyReLU(alpha=0.2)(layer) 
+    
+    #Growing layers
+    for i in range(self.layers):
+        layer = UpSampling2D(2)(layer)
+        layer = Conv2D(64, (3,3), padding='same', weights = self.genr_weights[i], name = 'genr_layer_'+str(i))(layer)
+        layer = LeakyReLU(alpha=0.2)(layer) 
+    
+    
+    layer = Conv2D(3, (1,1))(layer)
+    return Model(input_layer, layer)
+    
+def build_discriminator(self):
+    input_layer = Input(shape=self.inp_shape)
+    layer = input_layer
+    
+    layer = Conv2D(64, (1,1))(layer)
+    layer = LeakyReLU(alpha=0.2)(layer) 
+    
+    #Growing layers
+    for i in range(self.layers):
+        layer = Conv2D(64, (3,3), padding='same', weights = self.disc_weights[i], name = 'disc_layer_'+str(i))(layer)
+        layer = LeakyReLU(alpha=0.2)(layer)
+        layer = AveragePooling2D(2)(layer)
+    
+    layer = Conv2D(self.latent_dim, (4,4), padding='valid', weights = self.disc_head_weights, name = 'disc_head')(layer)
+    layer = LeakyReLU(alpha=0.2)(layer) 
+    layer = Flatten()(layer)
+    layer = Dense(1, activation=self.disc_activation)(layer)
+    
+    return Model(input_layer, layer)
+
 
        
 noise_dim = 100    
@@ -41,10 +81,13 @@ def sample_images(gen, file):
     
     
 img_path = 'ProgGAN'
-mode = 'vanilla'
+mode = 'stable'
     
 # Load the dataset
-(X_train, _), (_, _) = cifar10.load_data()
+(X_train, labels), (_, _) = cifar10.load_data()
+
+indx = np.where(labels == 1)[0]
+X_train = X_train[indx]
 
 # Configure input
 X_train = (X_train.astype(np.float32) - 127.5) / 127.5
@@ -54,8 +97,8 @@ if len(X_train.shape)<4:
 
 #Run GAN for 20000 iterations
 gan = ProgGAN(X_train.shape[1:], noise_dim, mode = mode)
-#gan.build_generator = lambda self=gan: model.build_generator(self)
-#gan.build_discriminator = lambda self=gan: model.build_discriminator(self)
+gan.build_generator = lambda self=gan: build_generator(self)
+gan.build_discriminator = lambda self=gan: build_discriminator(self)
 gan.build_models()
 
 def callback():
@@ -63,4 +106,4 @@ def callback():
     sample_images(gan.generator, path+'.png')
     plotter.save_hist_image(gan.history, path+'_hist.png')
     
-gan.train(X_train, epochs=20000, epochs_grow_rate = 200, batch_size=64, checkpoint_callback = callback, validation_split = 0.1)    
+gan.train(X_train, epochs=20000, batch_size=64, checkpoint_callback = callback, validation_split = 0.1)    
