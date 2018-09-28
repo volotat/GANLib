2cd0b0e73ca489c06d640844a92bddf615abfe26
Alex K <BangbangfmGames@gmail.com>
2018-09-29 00:17:32 +0300

    Update __init__.py

--- GANLib/__init__.py
+++ GANLib/__init__.py
@@ -9,10 +9,5 @@ from .GANs.GAN import GAN #main class
 from .GANs.AAE import AAE
 from .GANs.CGAN import CGAN
 from .GANs.DiscoGAN import DiscoGAN
-<<<<<<< HEAD
-from .GANs.GAN import GAN
-from .GANs.WGAN_GP import WGAN_GP
-=======
->>>>>>> Tensorflow
 
 __version__ = '0.0.6'
